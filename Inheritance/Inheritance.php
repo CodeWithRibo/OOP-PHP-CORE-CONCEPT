bccbb0892bbe41d4ba6e756b43d659a5748813e1
CodeWithRibo <riboluna2818@gmail.com>
2025-03-30 10:38:08 +0800

    added override method

--- Inheritance/Inheritance.php
+++ Inheritance/Inheritance.php
@@ -3,6 +3,8 @@
 spl_autoload_register(function($className){
         include "{$className}" . '.php';
 });
+
+
 #Physical Book
 $physicalBook = new PhysicalBook(title: 'Boruto', author: 'Ukyō Kodachi', price: 350.00, weight: 200);
 echo $physicalBook ->  displayDetails() . PHP_EOL;
