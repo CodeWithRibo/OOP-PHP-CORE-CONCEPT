6a128c05492fe9847d6f47548594b9cc3a02cb71
CodeWithRibo <riboluna2818@gmail.com>
2025-04-01 18:54:42 +0800

    added the speed to default value 0

---
+++ MachineProblem/SystemUnit/SystemComponents.php
@@ -0,0 +1,44 @@
+<?php
+
+#Parent Class with implementing Abstract
+abstract class SystemComponents
+{
+    #implement abstract for hiding unnecessary details
+    abstract public function getStatus(): string; 
+    abstract public function getDetails(): string; 
+
+    #added object property that inherit to subclass
+    protected $type;
+    protected $capacity;
+    protected $speed;
+    protected static $status = 'Still processing';
+
+    #set a value property from a parameter
+    public function __construct(string $type, int $capacity, float $speed = 0)
+    {
+        $this -> type = $type;
+        $this -> capacity = $capacity;
+        $this -> speed = $speed;
+    }
+
+    #retrieve the the value using access controll
+    public static function status(): string
+    {
+        return static::$status;
+    }
+
+    public function type():string
+    {
+        return $this->type;
+    }
+
+    public function capacity():int 
+    {
+        return $this->capacity;
+    }
+
+    public function speed():float
+    {
+        return $this->speed;
+    }
+}
